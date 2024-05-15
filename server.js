dc18e3543b5625f5ae5234d67d2c54cc06e6b199
Patryk Bombik <patryk.bombik@gmail.com>
2024-05-15 13:19:28 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/snippet-code-sender");
 
 app.get('/', (req, res) => {
+    const code = `Welcome to Snippet Code Sender!
+    
+Use the commands in the top right corner
+to send code snippets to your friends!`;
 
-    res.render('code-display', { code });
-})
+    res.render('code-display', {code});
+});
+
+app.get('/new', (req, res) => {
+    res.render("new");
+});
+
+app.post('/save', (req, res) => {
+    const value = req.body.value;
+    console.log(value)
+});
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
