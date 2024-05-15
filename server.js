eda72054b9db8aba73ee5eb3fa48af7bdbfd4870
Patryk Bombik <patryk.bombik@gmail.com>
2024-05-15 14:19:29 +0200

    models and api done

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
+const Document = require('./models/Document');
 const mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost/snippet-code-sender");
 
@@ -13,16 +14,42 @@ app.get('/', (req, res) => {
 Use the commands in the top right corner
 to send code snippets to your friends!`;
 
-    res.render('code-display', {code});
+    res.render('code-display', {code, language: "plaintext"});
 });
 
 app.get('/new', (req, res) => {
     res.render("new");
 });
 
-app.post('/save', (req, res) => {
+app.post('/save', async (req, res) => {
     const value = req.body.value;
-    console.log(value)
+    try {
+        const document = await Document.create({ value });
+        res.redirect(`/${document.id}`)
+    } catch (err) {
+        res.render('new', {value})
+    }
+});
+
+app.get('/:id/duplicate', async (req, res) => {
+  const id = req.params.id;
+  try {
+        const document = await Document.findById(id);
+        res.render('new', { value: document.value });
+  } catch (err) {
+      res.redirect(`/${id}`);
+  }
+})
+
+app.get('/:id', async (req, res) => {
+   const id = req.params.id;
+   try {
+         const document = await Document.findById(id);
+
+         res.render('code-display', { code: document.value, id });
+   } catch (err) {
+       res.redirect('/');
+   }
 });
 
 app.listen(3000, () => {
